6025cdad9ae63215eaf0e75a8dd0edd1af01f73c
amnamaharani <amnamaharani10@gmail.com>
2023-02-17 20:14:32 +0100

    day 4 page 2

--- lib/pages/second_empty.dart
+++ lib/pages/second_empty.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sevendays/widgets/theme.dart';
 
 class SecondEmpty extends StatelessWidget {
   const SecondEmpty({Key? key}) : super(key: key);
@@ -6,10 +7,30 @@ class SecondEmpty extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xff1B1B33),
       body: Column(
         children: [
           Image.asset(
             'assets/icon_chart.png',
+            height: 454,
+            width: 375,
+          ),
+          const SizedBox(height: 68,),
+          Text(
+            'Boost Profit',
+            style: titleTextStyle,
+          ),
+          const SizedBox(height: 16,),
+          Text(
+            'Our tools are helping business\nto grow much faster',
+            style: descTextStyle,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 59,),
+          Image.asset(
+            'assets/rocket_btn.png',
+            width: 65,
+            height: 65,
           )
         ],
       ),
