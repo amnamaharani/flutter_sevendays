983ff6530627343fcc9918bc62d45734b9f985f9
amnamaharani <amnamaharani10@gmail.com>
2023-02-17 19:46:39 +0100

    add constructor

--- lib/pages/second_signin.dart
+++ lib/pages/second_signin.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class SecondSignin extends StatefulWidget {
+  const SecondSignin({Key? key}) : super(key: key);
+
   @override
   State<SecondSignin> createState() => _SecondSigninState();
 }
